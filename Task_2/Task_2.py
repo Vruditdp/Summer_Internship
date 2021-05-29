1ce6c6bb20321eaeececc729ecebf80d0ba88c5d
Vruditdp <55894200+Vruditdp@users.noreply.github.com>
2021-05-29 11:04:48 +0530

    Task 2
    Task 2

---
+++ Task_2/Task_2.py
@@ -0,0 +1,37 @@
+'''This is example of comment'''
+print('Hello , I am Vrudit Patel\n\n')
+
+# example of variable:
+v1,v2,v3=2,3.4,'Vrudit'
+c=2+3j
+l=[2,20.5,'vrudit',55,23.2,'patel']
+name='Vruditkumr Patel'
+
+print('v1 is ',v1,type(v1))
+print('v2 is ',v2,type(v2))
+print('v3 is ',v3,type(v3))
+print('c is ',c,type(c))
+print('l is ',l,type(l))
+
+print('\n v2 is \n',isinstance(v2,int))
+
+print(name)
+print(name[2])
+print(name[1:5])
+print(name[3:])
+print(name[:6])
+print(name*2)
+print('\n\n')
+print(l)
+print(l[2])
+print(l[1:5])
+print(l[3:])
+print(l[:6])
+print(l*2)
+
+#example of dictionary
+
+d={'a1':2,2:5,'name':'vrudit'}
+print('a1 is ',d['a1'])
+print('name is ',d['name'])
+print('2 is ',d[2])
