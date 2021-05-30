44acc7f3e36f210d47c0643e0799508af604b6a8
Vruditdp <55894200+Vruditdp@users.noreply.github.com>
2021-05-30 13:05:19 +0530

    Task 3
    Task 3.1&3.2

---
+++ Task_3/Task_3-1.py
@@ -0,0 +1,64 @@
+# Que.1 Calculate average of 5 numbers.
+# print('Enter 5 numbers: ')
+# a1 = int(input("enter num 1  "))
+# a2 = int(input("enter num 2  "))
+# a3 = int(input("enter num 3  "))
+# a4 = int(input("enter num 4  "))
+# a5 = int(input("enter num 5  "))
+
+# print('The average of 5 numbers is : ',(a1+a2+a3+a4+a5)/5)
+
+# ##########################################################################
+# Check whether the entered nuber is odd or even.
+# n=int(input('Please enter a number : '))
+# if n%2==0 :
+#     print('Number is even.')
+# else:
+#     print('Number is Odd')
+#############################################################################
+# Check the number is zero, positive or negative.
+# if n==0:
+#     print('number is 0.')
+# elif n>0:
+#     print('Number is positive.')
+# else:
+#     print('Numberis negative.')   
+
+#################################################################################
+# Displaying greatest number among 2 numbers.
+             
+# n1=int(input("Please enter first num : "))
+# n2=int(input("Please enter second num : "))
+# if n1==n2:
+#     print('Numbers are equal.')
+# elif n1>n2 :
+#     print(n1,' is greater than ',n2)
+# else:
+#     print(n2,' is greater than ',n1)  
+
+########################################################################################
+# To find out Factorial of entered number from user.
+# num = int(input("Enter a number: "))    
+# factorial = 1    
+# if num < 0:    
+#    print(" Factorial does not exist for negative numbers")    
+# elif num == 0:    
+#    print("The factorial of 0 is 1")    
+# else:    
+#    for i in range(1,num + 1):    
+#        factorial = factorial*i    
+#    print("The factorial of",num,"is",factorial)
+# 
+################################################################################
+# Swap the value of two number using third variable.
+
+x = input('Enter value of x: ')
+y = input('Enter value of y: ')
+
+temp = x
+x = y
+y = temp 
+print('The value of x after swapping: ',x)
+print('The value of y after swapping: ',y)
+          
+
